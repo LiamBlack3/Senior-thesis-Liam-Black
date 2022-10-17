b77de1e764a2fc71b67e02b4f62041263f4a7ee4
Liam Black <blackl@allegheny.edu>
2022-10-17 09:40:49 -0400

    Update python file to new data

--- src/project.py
+++ src/project.py
@@ -7,17 +7,17 @@ myQuery_str2 = ""
 myQuery_str3 = ""
 myQuery_str4 = ""
 myQuery_str5 = ""
-myTable = "nba_handtracking"
+myTable = "team_stats"
 db_connect = "project.sqlite3" # name of the current database
 conn = sqlite3.connect(db_connect)
 
 
 def menu():
 	""" menu function to ask user what function is to be performed"""
-	options_list = [1, 2, 3, 4, 5, 6, 7] # can add more if more queries are needed
+	options_list = [1, 2, 3, 4, 5] # can add more if more queries are needed
 
 	#prmpt_str = f"\n\t 1 = To show everything in the Table\n\t 2 = To show the points per touch of the champions in the Table\n\t 3 = query2(add query)\n\t 4 = Show Point, Minute and Touch data per player\n\t 5 = Display Time of Possession and Points per Player\n\t 6 = Enter a custom SQL query\n\t Enter any number not listed to exit\n\t\t Enter integer :"
-	prmpt_str = f"\n\t 1 = To show everything in the Table\n\t 2 = To show the points per touch of the champions in the Table\n\t 3 = This option shows average paint touches per indivdual and their total wins\n\t 4 = Show Point, Minute and Touch data per player\n\t 5 = Display Time of Possession and Points per Player\n\t 6 = This shows players average seconds per touch and their scoring averages\n\t 7 = Enter a custom SQL query\n\t Enter any number not listed to exit\n\t\t Enter integer :"
+	prmpt_str = f"\n\t 1 = To show everything in the Table\n\t 2 = To show the points per touch of the champions in the Table\n\t 3 = This option shows average paint touches per indivdual and their total wins\n\t 4 = Show Point, Minute and Touch data per player\n\t 5 = Enter a custom SQL query\n\t  Enter any number not listed to exit\n\t\t Enter integer :"
 
 	# Each attribute is assigned a number. Show the attributes and allow user to choose a number.
 
@@ -44,10 +44,6 @@ def menu():
 	if result_int == 4:
 		question4(myQuery_str3, conn) # call this function if result_int is 4
 	if result_int == 5:
-		question5(myQuery_str4, conn) # call this function if result_int is 5
-	if result_int == 6:
-		question6(myQuery_str5, conn) # call this function if result_int is 6
-	if result_int == 7:
 		custom_query(conn)
 	else:
 		exit = True
@@ -137,47 +133,6 @@ def question4(in_str, conn):
 	main()
 	# end of question4()
 
-def question5(in_str, conn):
-	""" print a welcome message for option 2, QUERY"""
-	print("\n\t [+] Welcome to QUERY()")
-
-	#########################
-	# Query data in a table
-	myQuery_str4 = f"SELECT player, Time_of_Poss, PTS FROM {myTable} WHERE Time_of_Poss > 1"
-
-	print(f" [+] Running Query:\n {myQuery_str4}") #run the query
-	# Get result from query
-	result = conn.execute(myQuery_str4)
-	print(f" [+] Results from query: ")
-	queryInfo_list = result.fetchall()
-
-	for i in queryInfo_list:
-		print(f"\t[+] {i}")
-
-	main()
-	# end of question5()
-
-def question6(in_str, conn):
-	""" print a welcome message for option 2, QUERY"""
-	print("\n\t [+] Welcome to QUERY()")
-	print("\n\t [+] This shows players average seconds per touch and their scoring averages")
-	
-	#########################
-	# Query data in a table
-	myQuery_str5 = f"select player, time_of_poss, pts from {myTable}"
-
-	print(f" [+] Running Query:\n {myQuery_str5}") #run the query
-	# Get result from query
-	result = conn.execute(myQuery_str5)
-	print(f" [+] Results from query: ")
-	queryInfo_list = result.fetchall()
-
-	for i in queryInfo_list:
-		print(f"\t[+] {i}")
-
-	main()
-	# end of query()
-
 def custom_query(conn):
 	""" Allow the user to enter a custom query """
 	print("\n\t [+] Table name: nba_handtracking")
@@ -196,10 +151,14 @@ def custom_query(conn):
 	main()
 	#end of custom_query()
 
+
+
+
+
 def main():
 	# the program actually starts here. The dictionary of table names tables_dict is a global variable and is accessible by all functions, as necessary.
 	exit = False
-	print("\t Welcome to NBA Statistics! ")
+	print("\t Welcome to NBA Team Statistics! ")
 	
 	while exit == False:
 		result_int = menu()
